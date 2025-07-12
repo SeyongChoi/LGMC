4ce44af3cc6f02fe99a94b0140586ab3663cbb02
SeyongChoi <choiseyong6014@gmail.com>
2025-07-13 00:31:29 +0900

    setup

--- setup.py
+++ setup.py
@@ -13,6 +13,28 @@ extensions = [
 
 setup(
     name="lgmc",
-    packages=["lgmc", "lgmc.dynamics", "lgmc.init", "lgmc.utils"],
+    version='0.1',
+    
+    packages=["lgmc", "lgmc.dynamics", "lgmc.init", "lgmc.simulator", "lgmc.utils"],
+
+    # Cython extensions
     ext_modules=cythonize(extensions, language_level=3),
+    
+    # # Dependencies
+    # install_requires=[
+    #     "numpy>=1.21.0",
+    #     "tqdm",
+    #     "scipy",
+    #     # 필요 시 추가
+    # ],
+
+    # CLI entry point
+    entry_points={
+        'console_scripts': [
+            'lgmc = lgmc.main:main',  # lgmc/main.py 또는 main 함수
+        ]
+    },
+
+    include_package_data=True,
+    zip_safe=False,
 )
